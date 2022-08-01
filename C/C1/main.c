d3ed75d3921da075f461277ad44c5fc510b35830
Roman <109632797+IP2035prod@users.noreply.github.com>
2022-08-01 16:06:34 +0300

    Add files via upload

---
+++ C/C1/main.c
@@ -0,0 +1,26 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
+C#, OCaml, VB, Perl, Swift, Prolog, Javascript, Pascal, HTML, CSS, JS
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+#include <math.h>
+
+int module(int in)
+{
+    return(fabs(in)); //fabs - нахождение модуля от числа из <math.h>.
+}
+
+int main()
+{
+    int in;
+    int out;
+    printf("Введите целое число.\n");
+    scanf("%d", &in);
+    out = module (in);
+    printf("Модуль числа %d = %d. \n", in, out);
+    return 0;
+}
